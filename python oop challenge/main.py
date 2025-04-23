3050b3435ad454a91a758b950b4d281ab19618ff
Sarah-k984 <kiuriresarah@gmail.com>
2025-04-23 12:39:58 +0300

    Add files via upload

---
+++ python oop challenge/main.py
@@ -0,0 +1,14 @@
+from pet import cat
+acode = Cat("Acode")
+
+# Interact with your cat
+acode.get_status()
+acode.eat()
+acode.get_status()
+acode.play()
+acode.get_status()
+acode.sleep()
+acode.get_status()
+
+acode.train("sit")
+acode.show_tricks()
